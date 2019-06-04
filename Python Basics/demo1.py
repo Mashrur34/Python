b05af567e70600ea112f02a709d00bc421de1014
EequalsMC^2 <48718382+EequalsMCsquare@users.noreply.github.com>
2019-06-05 02:36:06 +1000

    Add files via upload

---
+++ Python Basics/demo1.py
@@ -0,0 +1,10 @@
+#First Program
+print('Hello Python')
+a = [1,2,"Text",True]
+print(type(a[3]))
+
+b = {1,2,3,3,"GOOD"}
+print(b)
+
+c = {"age":1, "Name": "EequalsMC^2","Python":True}
+print(c['age'])
